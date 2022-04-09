6646a58686c118cbfc9095e1a1699c4647e24d43
Yaroslaw <bruce.levitangens@gmail.com>
2022-04-09 12:42:01 +0300

    Add files via upload

--- Channel.cs
+++ Channel.cs
@@ -1,6 +1,35 @@
-﻿namespace TSMO;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public readonly record struct Channel(int CountSetup, double ProbabilityDefeat, double RateSetup)
+namespace TSMO;
+
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="CountSetup">Количество установок</param>
+/// <param name="ProbabilityDefeat">Вероятность поражения</param>
+/// <param name="RateSetup">Скорострельность каждой установки</param>
+public record struct Channel
 {
+    public int CountSetup { get; init; }
+    public double ProbabilityDefeat { get; init; }
+    public double RateSetup { get; init; }
+    public double ProcessingTime { get; init; }
+
+    public bool IsActive { get; set; }
+    public double? StartTime { get; set; }
+    public int? IndexRequest { get; set; }
+
+    /// <summary>
+    /// Считает скорострельность всего канала
+    /// </summary>
+    /// <returns>Скорострельность всего канала</returns>
     public double GetRateChannel() => CountSetup * ProbabilityDefeat * RateSetup;
+}
+
+public class ChannelComparerWithIndex : IEqualityComparer<Channel>
+{
+    public bool Equals(Channel x, Channel y) => x.IndexRequest == y.IndexRequest;
+
+    public int GetHashCode([DisallowNull] Channel obj) => obj.IndexRequest.GetHashCode();
 }
--- ChannelService.cs
+++ ChannelService.cs
@@ -9,15 +9,75 @@ namespace TSMO;
 public class ChannelService
 {
     private List<Channel> channels;
+    private Dictionary<int, double> requestsProcTime;
     private int countParalelChannels;
-
-    public delegate void UpdateHandler();
-    public event UpdateHandler? UpdateChannel;
 
     public ChannelService(int channelsCount, int countParalelChannels)
     {
         channels = new(channelsCount);
         this.countParalelChannels = countParalelChannels;
+
+        requestsProcTime = new();
     }
 
+    public void AddRequest(double activeTime, int indexRequest)
+    {
+        UpdateChannel(activeTime);
+
+        List<Channel> temp = channels.Where(obj => !obj.IsActive).ToList();
+        int countActive;
+
+        if (temp.Count >= countParalelChannels)
+            countActive = countParalelChannels;
+        else
+            countActive = temp.Count;
+
+        for (int i = 0; i < countActive; i++)
+        {
+            temp[i] = temp[i] with
+            {
+                IsActive = true,
+                IndexRequest = indexRequest,
+                StartTime = activeTime
+            };
+
+            UpdateProcTime(activeTime, indexRequest, temp[i].ProcessingTime);
+        }
+    }
+
+    private void UpdateProcTime(double activeTime, int indexRequest, double procTimeChannel)
+    {
+        foreach (KeyValuePair<int, double> proccTime in requestsProcTime.Where(procTime => !channels.Contains(new Channel() { IndexRequest = procTime.Key }, new ChannelComparerWithIndex())))
+        {
+            requestsProcTime.Remove(proccTime.Key);
+        }
+
+        if (requestsProcTime.ContainsKey(indexRequest))
+        {
+            requestsProcTime[indexRequest] -=
+                (activeTime - requestsProcTime[indexRequest]) / channels.
+                    Where(channel => channel.IndexRequest == indexRequest).
+                    Count();
+        }
+        else
+            requestsProcTime.Add(indexRequest, procTimeChannel);
+    }
+
+    private void UpdateChannel(double activeTime)
+    {
+        channels.ForEach(channel =>
+        {
+            if (channel.IsActive)
+            {
+                if (requestsProcTime[channel.IndexRequest.Value] + channel.StartTime <= activeTime)
+                {
+                    channel.IsActive = false;
+                    channel.StartTime = null;
+                    channel.IndexRequest = null;
+                }
+            }
+        });
+    }
 }
+
+
